35893c15c1e738d1ef4a84f75b4de93ac947b3ad
vasupatel <vasurabadiya5@gmail.com>
2023-07-30 12:23:44 +0530

    done

--- Assets/Scripts/NewSceneUI.cs
+++ Assets/Scripts/NewSceneUI.cs
@@ -10,6 +10,6 @@ public class NewSceneUI : MonoBehaviour
     {
         // Retrieve the last scene score from PlayerPrefs and display it.
         int lastSceneScore = PlayerPrefs.GetInt("LastSceneScore", 0);
-        scoreText.text = "Last Score: " + lastSceneScore.ToString();
+        scoreText.text = lastSceneScore.ToString();
     }
 }
