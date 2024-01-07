b527b06071de3700cd24f390525081c2da8c0850
vasupatel <vasurabadiya5@gmail.com>
2024-01-07 14:55:18 +0530

    final build

--- Assets/_2048 Game/Scripts/MainMenu.cs
+++ Assets/_2048 Game/Scripts/MainMenu.cs
@@ -27,31 +27,8 @@ public class MainMenu : MonoBehaviour
 
     public void StartGame()
     {
-
-        //SceneManager.LoadScene(firstLevel);
-        //For check internet
-        if (Application.internetReachability != NetworkReachability.NotReachable)
-        {
-            InterstitialAdController.OnUserClosed = null;
-            InterstitialAdController.OnUserClosed += () => { SceneManager.LoadScene("2048"); };
-            GoogleMobileAdsController.Instance.ShowInterstitialAdd();
-        }
-        else
-        {
-            SceneManager.LoadScene("2048");
-        }
+        SceneManager.LoadScene("2048");
     }
-
-    public void OpenOptions()
-    {
-        optionsScreen.SetActive(true);
-    }
-
-    public void CloseOptions()
-    {
-        optionsScreen.SetActive(false);
-    }
-
     public void QuitGame()
     {
         Application.Quit();
